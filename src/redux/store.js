a037218901f686875a88022aabbab0efd5026214
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-06-21 16:57:36 +0300

    Redux, react-redux initialised

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+
+const reducers = combineReducers({});
+
+const store = configureStore({reducer: reducers});
+
+export default store;
